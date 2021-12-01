f02177b24132c4075a526e64d24cf269bb724864
satou1497 <satou1497@gmail.com>
2021-12-01 10:25:18 +0900

    ファビコン変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/mount.ico' }
     ]
   },
 
